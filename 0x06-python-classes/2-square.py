23e0b10e516b3e6366523857e8abb7261df25f5e
aminamohsen <amina.mohsen4004@gmail.com>
2023-10-24 13:36:24 +0300

    python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,13 +2,13 @@
 
 """Define a class Square."""
 
-class Square:
 
+class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-
         """Initialize a new Square.
+
         Args:
             size (int): The size of the new square.
         """
